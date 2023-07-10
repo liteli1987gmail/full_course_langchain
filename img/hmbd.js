384441dd5a95184ede235b03a4720a84ad5a8f8f
liteli1987gmail <liteli1987@gmail.com>
2023-07-10 17:09:57 +0800

    提交要用到的图片

---
+++ img/hmbd.js
@@ -0,0 +1,9 @@
+(function() {
+      var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement("script");
+hm.src = "https://hm.baidu.com/hm.js?e60fb290e204e04c5cb6f79b0ac1e697";
+var s = document.getElementsByTagName("script")[0]; 
+s.parentNode.insertBefore(hm, s);
+})();
+    })();
